28542db46f10c819039e82c20eb19ad12e612723
Charles Webb <cgwebb.12@gmail.com>
2018-10-23 15:19:51 -0400

    made some minor changes and added to topic site

---
+++ kjtimeline.js
@@ -0,0 +1,8 @@
+var options = {
+    timenav_height_percentage: 40,
+    initial_zoom: 3.5
+};
+
+function createTimeline() {
+    timeline = new TL.Timeline('timeline-embed','https://docs.google.com/spreadsheets/d/1I0fxrEClWzyZeXgKOAijoihs7NoIFC3enNaT4GA-bkI/edit#gid=0', options);
+}
